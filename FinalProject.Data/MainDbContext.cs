ac6c6e8802e402f007e258964241ae89de9c64db
muratpala007 <muratpala007@gmail.com>
2022-06-14 14:02:46 +0300

    Data layer created. MainDbContext added. Entity Framework packages installed.

---
+++ FinalProject.Data/MainDbContext.cs
@@ -0,0 +1,28 @@
+﻿using FinalProject.Domain.Reservations;
+using FinalProject.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace FinalProject.Data
+{
+    public class MainDbContext : DbContext
+    {
+        public MainDbContext(DbContextOptions<MainDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+
+        //Model-level query to filter for Soft Deleting
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasQueryFilter(b => !b.IsDeleted);
+        }
+    }
+}
